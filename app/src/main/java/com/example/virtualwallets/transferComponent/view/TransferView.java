630f7a797da56c1b00f4a6cdcd362e70d2a045e9
cgcruz <cgcruz@gruposion.bo>
2019-11-13 10:03:11 -0400

    configuracion de de butternife y formularion para transferencia

--- app/src/main/java/com/example/virtualwallets/transferComponent/view/TransferView.java
+++ app/src/main/java/com/example/virtualwallets/transferComponent/view/TransferView.java
@@ -2,11 +2,13 @@ package com.example.virtualwallets.transferComponent.view;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.virtualwallets.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +22,28 @@ public class TransferView extends AppCompatActivity implements ITransferView {
 
     @BindView(R.id.toolbar_transfer)
     public Toolbar toolbar;
+
+    @BindView(R.id.edit_text_cuenta_origen)
+    public EditText origen;
+    @BindView(R.id.edit_text_destino)
+    public EditText destino;
+    @BindView(R.id.edit_text_list_destino)
+    public EditText listDestinos;
+    @BindView(R.id.edit_text_nro_cuenta)
+    public EditText nroCuenta;
+    @BindView(R.id.edit_text_monto)
+    public EditText monto;
+
+    @BindView(R.id.til_cuenta_origen)
+    public TextInputLayout tilOrigen;
+    @BindView(R.id.til_destino)
+    public TextInputLayout tilDestino;
+    @BindView(R.id.til_list_destino)
+    public TextInputLayout tilListDestion;
+    @BindView(R.id.til_nro_cuenta)
+    public TextInputLayout tilNroCuenta;
+    @BindView(R.id.til_monto)
+    public TextInputLayout tilMonto;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
