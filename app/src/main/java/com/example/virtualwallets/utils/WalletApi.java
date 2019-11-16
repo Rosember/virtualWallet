1690dc235c23bdc8d9a0a3ae97a968982dc36ca6
cgcruz <cgcruz@gruposion.bo>
2019-11-16 11:58:29 -0400

    agregando cambios de la master

--- app/src/main/java/com/example/virtualwallets/utils/WalletApi.java
+++ app/src/main/java/com/example/virtualwallets/utils/WalletApi.java
@@ -1,14 +1,11 @@
 package com.example.virtualwallets.utils;
 
 import com.example.virtualwallets.loginComponent.model.LoginResponse;
-import com.example.virtualwallets.transferComponent.model.WalletResponse;
-import com.example.virtualwallets.walletComponent.model.WalletsResponse;
-import com.example.virtualwallets.loginComponent.model.LogoutResponse;
-//import com.example.virtualwallets.mainComponent.model.WalletsResponse;
-import com.example.virtualwallets.transactionComponent.model.DaoTransaction;
 import com.example.virtualwallets.transactionComponent.model.TransactionResponse;
 import com.example.virtualwallets.transferComponent.model.TransferRequest;
 import com.example.virtualwallets.transferComponent.model.TransferResponse;
+import com.example.virtualwallets.transferComponent.model.WalletResponse;
+import com.example.virtualwallets.walletComponent.model.WalletsResponse;
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +18,8 @@ import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
+//import com.example.virtualwallets.mainComponent.model.WalletsResponse;
+
 /**
  * @autor Ing. Carlos G. Cruz Andia
  * Creado el 2019-11-13
@@ -28,21 +27,21 @@ import retrofit2.http.Path;
 public interface WalletApi {
 
     @POST("auth/login")
-    Observable<LoginResponse> loginSession(@Body HashMap<String,String> data);
+    Call<LoginResponse> loginSession(@Body HashMap<String, String> data);
 
     @POST("auth/logout")
-    Observable<Boolean> logout(@Header("Authorization") String toke, @Body HashMap<String,String> data);
+    Call<Boolean> logout(@Header("Authorization") String toke, @Body HashMap<String, String> data);
 
     @GET("users/{user}/wallets")
-    Observable<List<WalletsResponse>> listWallets(@Path("user") int userId, @Header("Authorization") String token);
+    Call<List<WalletsResponse>> listWallets(@Path("user") int userId, @Header("Authorization") String token);
 
 
     @GET("wallets/{wallet_id}/transactions")
     Call<List<TransactionResponse>> listTransactions(@Path("wallet_id") int wallet_id, @Header("Authorization") String token);
 
     @POST("transactions")
-    Call<TransferResponse> transfer(@Header("Authorization") String token,@Body TransferRequest request);
+    Call<TransferResponse> transfer(@Header("Authorization") String token, @Body TransferRequest request);
 
     @GET("wallets/by-number/{wallet_number_a}")
-    Call<WalletResponse> findByNumberWallet(@Path("wallet_number_a") String number,@Header("Authorization") String token);
+    Call<WalletResponse> findByNumberWallet(@Path("wallet_number_a") String number, @Header("Authorization") String token);
 }
