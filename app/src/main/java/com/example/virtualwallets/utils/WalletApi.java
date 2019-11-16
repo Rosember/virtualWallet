99d5225472883e59f3710aa42ae4bdabf87d6404
cgcruz <cgcruz@gruposion.bo>
2019-11-16 12:00:45 -0400

    ..

--- app/src/main/java/com/example/virtualwallets/utils/WalletApi.java
+++ app/src/main/java/com/example/virtualwallets/utils/WalletApi.java
@@ -10,7 +10,6 @@ import com.example.virtualwallets.walletComponent.model.WalletsResponse;
 import java.util.HashMap;
 import java.util.List;
 
-import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
